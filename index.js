d055df8a3e29f1b95da690210d3f18fbcc569a20
DisuAde <idrisadekunle726@gmail.com>
2024-06-01 20:50:36 +0100

    update cors access

--- index.js
+++ index.js
@@ -6,26 +6,32 @@ const routes = require("./routes/comments");
 
 require('dotenv').config()
 
-app.use(express.json())
+app.use(express.json());
+
+// CORS middleware
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
   next();
 });
+
 mongoose
   .connect(process.env.MONGO_URL)
-  .then((res) => { 
-    
-    app.listen(process.env.PORT, (req, res) => {
-      console.log("Started listening thank you",process.env.PORT);
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log("Started listening on port", process.env.PORT);
     });
   })
   .catch((err) => {
     console.log(err);
   });
 
-app.use("/api/comments",routes);
+app.use("/api/comments", routes);
 
 app.get("/", (req, res) => {
-  res.status(200).json({ msg: "Welcome to the rhytm" });
+  res.status(200).json({ msg: "Welcome to the rhythm" });
 });
