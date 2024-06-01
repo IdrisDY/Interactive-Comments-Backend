d018173e099c02e24466ed4d2674fd9216b58b26
DisuAde <idrisadekunle726@gmail.com>
2024-06-01 20:53:39 +0100

    update cors access add patch

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT,PATCH, DELETE, OPTIONS');
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
   }
